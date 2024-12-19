46ea75b0363066984d65521d054f58f86555193b
kagomee12 <kalfitrah@gmail.com>
2024-12-19 16:53:08 +0700

    feat: add index router

---
+++ src/routers/index.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import authRouter from "./authRouter";
+import userRouter from "./userRoutes";
+import wishListRouter from "./wishListRoutes";
+
+const router = Router();
+router.use("/auth", authRouter);
+router.use("user", userRouter);
+router.use("/wishlist", wishListRouter);
+
+export default router;
