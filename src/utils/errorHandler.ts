3d0b1e3008dbaf7441f9f57e2d4b4f3032a61e92
kagomee12 <kalfitrah@gmail.com>
2024-12-19 16:43:47 +0700

    feat: add errorhandler

---
+++ src/utils/errorHandler.ts
@@ -0,0 +1,11 @@
+import { Response } from "express";
+import ERROR_CODE from "./constants/errorCode";
+import ERROR_MESSAGE from "./constants/errorMessage";
+
+export default function errorHandler(res: Response, err: Error) {
+  let message = err.message;
+
+  return res
+    .status(ERROR_CODE[message] || 500)
+    .json({ error: ERROR_MESSAGE[message] || message });
+}
