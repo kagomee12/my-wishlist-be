18c6008aae0955d98b87c83f0eaabe299ed9bfd1
kagomee12 <kalfitrah@gmail.com>
2024-12-19 16:54:26 +0700

    chore: add router, cors, and configure Express to handle JSON and form data requests

--- index.ts
+++ index.ts
@@ -1,14 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import router from "./src/routers";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use(router);
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
