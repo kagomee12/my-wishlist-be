9ba2e101414c55d3558b3bf5e8d428f3f848902b
kagomee12 <kalfitrah@gmail.com>
2024-12-22 00:10:25 +0700

    chore: improve prettier code

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -2,31 +2,42 @@ import { editUser, getAllUsers, getUserById } from "../services/userService";
 import errorHandler from "../utils/errorHandler";
 
 export const getUser = async (req: any, res: any) => {
-    try {
-        const { id } = req.params;
-        const data = await getUserById(id);
-        res.status(200).json({ messages: "success", data });
-    } catch (error) {
-        errorHandler(res, error as unknown as Error);
-    }
+  try {
+    const { id } = req.params;
+    const data = await getUserById(id);
+    res.status(200).json({
+      status: "success",
+      data: data,
+      message: "get user successfully",
+    });
+  } catch (error) {
+    errorHandler(res, error as unknown as Error);
+  }
 };
 
 export const editUserbyId = async (req: any, res: any) => {
-    try {
-        const { id } = req.params;
-        const data = await editUser(id, req.body);
-        res.status(200).json({ messages: "success", data });
-    } catch (error) {
-        errorHandler(res, error as unknown as Error);
-    }
+  try {
+    const { id } = res.locals.user;
+    const data = await editUser(id, req.body);
+    res.status(200).json({
+      status: "success",
+      data: data,
+      message: "success edit profile",
+    });
+  } catch (error) {
+    errorHandler(res, error as unknown as Error);
+  }
 };
 
 export const getAllUser = async (req: any, res: any) => {
-    try {
-        const data = await getAllUsers();
-        res.status(200).json({ messages: "success", data });
-    } catch (error) {
-        errorHandler(res, error as unknown as Error);
-    }
+  try {
+    const data = await getAllUsers();
+    res.status(200).json({
+      status: "success",
+      data: data,
+      message: "success get all user",
+    });
+  } catch (error) {
+    errorHandler(res, error as unknown as Error);
+  }
 };
-
